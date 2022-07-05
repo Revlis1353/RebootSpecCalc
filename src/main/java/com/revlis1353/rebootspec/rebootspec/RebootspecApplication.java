1e8bd9e595549d773e2120a64b08ba21392339ec
Revlis1353 <chan00365@naver.com>
2022-07-05 16:32:37 +0900

    Initial commit

---
+++ src/main/java/com/revlis1353/rebootspec/rebootspec/RebootspecApplication.java
@@ -0,0 +1,13 @@
+package com.revlis1353.rebootspec.rebootspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebootspecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebootspecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revlis1353/rebootspec/rebootspec/RebootspecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revlis1353.rebootspec.rebootspec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebootspecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
